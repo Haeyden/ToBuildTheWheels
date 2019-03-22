8db8692be310424687ec987e4a6a5aaba20cd39d
Haeyden <micomecy@163.com>
2019-03-22 11:20:24 +0800

    Hel

---
+++ filetransform/src/main/java/com/micomecy/filetransform/FiletransformApplication.java
@@ -0,0 +1,13 @@
+package com.micomecy.filetransform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiletransformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiletransformApplication.class, args);
+    }
+
+}
---
+++ filetransform/src/test/java/com/micomecy/filetransform/FiletransformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.micomecy.filetransform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FiletransformApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
